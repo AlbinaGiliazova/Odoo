79f2a256b4049032837a49460aef7c481f610886
Albina Giliazova <giliazova@mail.ru>
2025-09-02 18:40:59 +0300

    Initial

---
+++ addons/packing/__manifest__.py
@@ -0,0 +1,12 @@
+{
+      'name': 'Packing',
+      'version': '1.0',
+      'category': 'Warehouse',
+      'summary': 'Модуль для управления упаковкой',
+      'description': 'Добавляет базовые функции упаковки товаров',
+      'author': 'Альбина Гилязова',
+      'license': 'MIT',
+      'depends': ['base'],
+      'data': [],
+      'installable': True,
+  }
